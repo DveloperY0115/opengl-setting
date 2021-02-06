1b08ee2bd2aff8c0364db0417ea5f3b28bd7d792
SeungWoo Yoo <dreamy1534@gmail.com>
2021-02-06 15:24:11 +0900

    feat(main): Build configuration on MSVC

--- main.cpp
+++ main.cpp
@@ -6,15 +6,19 @@
 #define GL_SILENCE_DEPRECATION
 #endif
 
+#ifdef _MSC_VER
+#include <GL/glut.h>
+#endif
+
 int width = 400;
 int height = 400;
 
 void display() {
-    /*
+
 #ifdef __APPLE__
     glViewport(0, 0, 2 * width, 2 * height);
 #endif
-     */
+
     glClear(GL_COLOR_BUFFER_BIT);
     glBegin(GL_POLYGON);
     glVertex3f(-0.5, -0.5, 0.0);
